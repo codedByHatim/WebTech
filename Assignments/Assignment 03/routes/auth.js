49e51bb4f5422c796d27795d96137cff137fcfc2
Hatim Khalid <inbox.hatim@gmail.com>
2025-06-18 14:51:12 +0500

    .

--- Assignments/Assignment 03/routes/auth.js
+++ Assignments/Assignment 03/routes/auth.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
+// Register Page
 router.get('/register', (req, res) => {
     res.render('register');
 });
@@ -14,23 +11,36 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
+        const exists = await User.findOne({ username });
+        if (exists) return res.send('User already exists');
+
         const user = new User({ username, password });
         await user.save();
         res.redirect('/auth/login');
     } catch (err) {
-        res.send('Error creating user: ' + err.message);
+        res.send('Error: ' + err.message);
     }
+});
+
+// Login Page
+router.get('/login', (req, res) => {
+    res.render('login');
 });
 
 router.post('/login', async (req, res) => {
     const { username, password } = req.body;
     const user = await User.findOne({ username });
-    if (!user) return res.send('No user found');
-    
+    if (!user) return res.send('User not found');
+
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.send('Incorrect password');
+    if (!isMatch) return res.send('Wrong password');
 
     req.session.user = user;
+    res.redirect('/');
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy();
     res.redirect('/');
 });
 
